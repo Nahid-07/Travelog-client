f21176b9f5da844d225d99f60272e73c672a522f
Nahid-07 <nahidasif1998@gmail.com>
2022-11-08 02:31:36 +0600

    added another section on home page

--- src/Components/Home/Article2.js
+++ src/Components/Home/Article2.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import img1 from '../../assets/chandranath.jpg'
+import img2 from '../../assets/jharna.jpg'
 
 const Article2 = () => {
     return (
         <section className="">
 	<div className="container max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
 		<div>
-			<h2 className="text-3xl font-bold tracking-tight text-center sm:text-5xl ">Aliquip definiebas ad est</h2>
-			<p className="max-w-3xl mx-auto mt-4 text-xl text-center ">Quando cetero his ne, eum admodum sapientem ut.</p>
+			<h2 className="text-3xl font-bold tracking-tight text-center sm:text-5xl ">Stay with Tra<span className='text-orange-700'>Vlog</span></h2>
+			<p className="max-w-3xl mx-auto mt-4 text-xl text-center ">Every tour has its separate memory.keep rock and more trip with Travlog</p>
 		</div>
 		<div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
 			<div>
-				<h3 className="text-2xl font-bold tracking-tight sm:text-3xl dark:text-gray-50">Ad vix debet docendi</h3>
-				<p className="mt-3 text-lg ">Ne dicta praesent ocurreret has, diam theophrastus at pro. Eos etiam regione ut, persius eripuit quo id. Sit te euismod tacimates.</p>
+				<h3 className="text-2xl font-bold tracking-tight sm:text-3xl dark:text-gray-50">Chandranath at Chottogram</h3>
+				<p className="mt-3 text-lg ">Chandranath hill is the right place for those who likes Climbing. The small hill group of Chandranath starts from Bichkunda. On the way to Chandranath hill you will see some of the religious places of Hindus religion.</p>
 				<div className="mt-12 space-y-12">
 					<div className="flex">
 						<div className="flex-shrink-0">
@@ -22,8 +24,7 @@ const Article2 = () => {
 							</div>
 						</div>
 						<div className="ml-4">
-							<h4 className="text-lg font-medium leading-6 ">Per ei quaeque sensibus</h4>
-							<p className="mt-2 ">Ex usu illum iudico molestie. Pro ne agam facete mediocritatem, ridens labore facete mea ei. Pro id apeirian dignissim.</p>
+							<p className="mt-2 ">Chittagong is on the direct route from Dhaka. There is available A/C & none A/C bus service 24 hours a day. U can also use train or Air (Biman). First you have to come to Sitakunda from Chittagong by Bus</p>
 						</div>
 					</div>
 					<div className="flex">
@@ -35,8 +36,8 @@ const Article2 = () => {
 							</div>
 						</div>
 						<div className="ml-4">
-							<h4 className="text-lg font-medium leading-6 dark:text-gray-50">Cu imperdiet posidonium sed</h4>
-							<p className="mt-2 ">Amet utinam aliquando ut mea, malis admodum ocurreret nec et, elit tibique cu nec. Nec ex maluisset inciderint, ex quis.</p>
+							
+							<p className="mt-2 ">Sitakunda Bazar. Chandranath hill stands 3.5 kilometers east from Sitakunda Bazar. You can go Chandranath hill side with a rickshaw or on foot.</p>
 						</div>
 					</div>
 					<div className="flex">
@@ -48,21 +49,20 @@ const Article2 = () => {
 							</div>
 						</div>
 						<div className="ml-4">
-							<h4 className="text-lg font-medium leading-6 dark:text-gray-50">Nulla omittam sadipscing mel ne</h4>
-							<p className="mt-2 ">At sed possim oporteat probatus, justo graece ne nec, minim commodo legimus ut vix. Ut eos iudico quando soleat, nam modus.</p>
+							<p className="mt-2 ">Dhaka and Chittagong are linked by road. You can take a bus from Dhaka to reach the district of Chittagong. Some of the bus services are listed below for our assistance.</p>
 						</div>
 					</div>
 				</div>
 			</div>
 			<div aria-hidden="true" className="mt-10 lg:mt-0">
-				<img src="https://source.unsplash.com/random/360x480" alt="" className="mx-auto rounded-lg shadow-lg dark:bg-gray-500" />
+				<img src={img1} alt="" className="mx-auto rounded-lg shadow-lg dark:bg-gray-500" />
 			</div>
 		</div>
 		<div>
 			<div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
 				<div className="lg:col-start-2">
-					<h3 className="text-2xl font-bold tracking-tight sm:text-3xl dark:text-gray-50">Eam nibh gloriatur ex</h3>
-					<p className="mt-3 text-lg ">Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine theophrastus. Ea his tale nibh dissentias, mei exerci tamquam euripidis cu.</p>
+					<h3 className="text-2xl font-bold tracking-tight sm:text-3xl dark:text-gray-50">Khoiyachora at Feni</h3>
+					<p className="mt-3 text-lg ">It is one of the largest waterfalls in Bangladesh. It has 9 amazing steps that makes it unique. Really beautiful atmosphere with good flow of water and green forests around. But the trail to this fall is a but risky. So travelers must be careful. Highly recommended for the advnture lovers.</p>
 					<div className="mt-12 space-y-12">
 						<div className="flex">
 							<div className="flex-shrink-0">
@@ -73,8 +73,7 @@ const Article2 = () => {
 								</div>
 							</div>
 							<div className="ml-4">
-								<h4 className="text-lg font-medium leading-6 dark:text-gray-50">Cibo augue offendit has ad</h4>
-								<p className="mt-2 ">An per velit appellantur, ut utinam minimum nominavi sit, odio nostro habemus ne nec. Ne sonet regione contentiones est.</p>
+								<p className="mt-2 ">To visit Khoiya Chora waterfall you will have to get off from the bus at Bartakiya bazaar, Mirsharai on Dhaka-Chittagong highway. Then take a CNG from Bartakiya to Rail gate.</p>
 							</div>
 						</div>
 						<div className="flex">
@@ -86,8 +85,7 @@ const Article2 = () => {
 								</div>
 							</div>
 							<div className="ml-4">
-								<h4 className="text-lg font-medium leading-6 dark:text-gray-50">At eum ferri luptatum lobortis</h4>
-								<p className="mt-2 ">Te per quidam maiorum ocurreret, etiam delicatissimi usu ad. Ne has quod periculis. Te sit primis iisque efficiantur.</p>
+								<p className="mt-2 "> Then just follow the road, after a while you will see the Chara/Jiri Path (water stream,) now follow this chara.</p>
 							</div>
 						</div>
 						<div className="flex">
@@ -99,14 +97,14 @@ const Article2 = () => {
 								</div>
 							</div>
 							<div className="ml-4">
-								<h4 className="text-lg font-medium leading-6 dark:text-gray-50">Dicunt verterem evertitur eu sea</h4>
-								<p className="mt-2 ">Audire principes rationibus eam an, autem nominavi luptatum per te. Sumo fabulas vim eu, sonet saperet eleifend ut vix.</p>
+								
+								<p className="mt-2 ">There is a local man made footpath which is following the Chara/Jiri path. So use the footpath, after half an hour you will see some houses. Then use your right hand path which will lead to the waterfall.</p>
 							</div>
 						</div>
 					</div>
 				</div>
 				<div className="mt-10 lg:mt-0 lg:col-start-1 lg:row-start-1">
-					<img src="https://source.unsplash.com/random/361x481" alt="" className="mx-auto rounded-lg shadow-lg dark:bg-gray-500" />
+					<img src={img2} alt="" className="mx-auto rounded-lg shadow-lg dark:bg-gray-500" />
 				</div>
 			</div>
 		</div>
